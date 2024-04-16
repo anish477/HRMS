c60321961a306403d37c456c370b4a1522b91d72
anish477 <anishsharma6798@gmail.com>
2024-04-16 18:51:49 +0545

    admin dashBoard

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/workouts", workoutRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/categories", userRoutes);
 
 // connect to db
 mongoose
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,11 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import Employee from "./components/Employee";
+import Category from "./components/Category";
+import Profile from "./components/Profile";
+import AddCategory from "./components/AddCategory";
 function App() {
   const { user } = useAuthContext();
   return (
@@ -16,7 +21,7 @@ function App() {
           <Routes>
             <Route
               path="/"
-              element={user ? <Home /> : <Navigate to="/login" />}
+              element={user ? <Dashboard /> : <Navigate to="/login" />}
             />
             <Route
               path="/login"
@@ -26,6 +31,30 @@ function App() {
               path="/signup"
               element={!user ? <Signup /> : <Navigate to="/" />}
             />
+            <Route
+              path="/dashboard"
+              element={
+                user ? <Dashboard /> : <Navigate to="/dashboard"></Navigate>
+              }
+            />
+
+            <Route path="" element={<Home />}></Route>
+            <Route path="/dashboard/employee" element={<Employee />}></Route>
+
+            <Route path="/dashboard/category" element={<Category />}>
+              <Route
+                path="add_category"
+                element={<h1></h1>}
+                // <Home />}
+              ></Route>
+            </Route>
+
+            <Route path="/dashboard/profile" element={<Profile />}></Route>
+            <Route
+              path="/dashboard/add_category"
+              element={<AddCategory />}
+            ></Route>
+            <Route path="/dashboard/add_employee" element={<Home />}></Route>
           </Routes>
         </div>
       </BrowserRouter>
